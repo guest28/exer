05ee871e4344578c1db85b4e4dd226de36af47f8
guest28 <105896881+guest28@users.noreply.github.com>
2022-05-20 15:46:32 +0900

    Add files via upload

---
+++ src/webapp/resources/js/admin_order.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+function go_order_save() {
+	
+	var count = 0;
+	if(documnet.frm.result.length == undefined) {
+		
+		if(document.frm.result.checked == true) {
+			
+			count++;
+		}
+	} else {
+		
+		
+	}
+	
+	
+}
